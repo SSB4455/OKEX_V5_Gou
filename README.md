d10eb1bed0387888bcf4293179b315c8e7ac67e8
SSB4455 <741086@gmail.com>
2021-05-07 18:27:56 +0800

    优化小狗策略

--- README.md
+++ README.md
@@ -1,18 +1,1 @@
-OKEx Open API V3 SDK
-
-### API Support 
-
-|联系我们|Contact Us||
-|---|---|---|
-|微信号|WeChat ID|ApiSupport|
-
-### Supported language: Java,  Python, C#, C++, Go
----
-
-|list|language|comment|
-|---|---|---|
-|okex-java-sdk-api|Java|-|
-|okex-python-sdk-api|Python|-|
-|okex-cs-sdk-api|C#|-|
-|okex-cpp-sdk-api|C++|-|
-|okex-go-sdk-api|Go|-|
+By OKEx Open API V5 SDK
--- okex-python-sdk-api/gou_dog.py
+++ okex-python-sdk-api/gou_dog.py
@@ -13,45 +13,49 @@ instId = 'DOGE-USDT'
 buy_price = -1
 current_price = None
 deal_side = 'buy'
-loss_limite = 0.009
-gain_limite = 0.018
+upl_ratio = 0
+loss_limite = -0.012
+gain_limite = 0.009
 losted_times = 0
+leverage = 3
 
 
 def init(api_key, api_secret_key, passphrase, instId2):
-    global accountAPI, spotAPI, levelAPI, gouId, instId
+    global accountAPI, spotAPI, levelAPI, gouId, instId, leverage, loss_limite, gain_limite
     accountAPI = account.AccountAPI(api_key, api_secret_key, passphrase, False)
     spotAPI = spot.SpotAPI(api_key, api_secret_key, passphrase, False)
     levelAPI = lever.LeverAPI(api_key, api_secret_key, passphrase, False)
     instId = instId2
-    levelAPI.set_leverage(instId, 3, 'cross', 'USDT')
+    levelAPI.set_leverage(instId, leverage, 'cross', 'USDT')
     gouId = instId + '_' + datetime.datetime.now().replace(microsecond=0).isoformat("T").replace(':', '')
-    record_to_file("gou_init\t" + instId + '\t' + str(loss_limite) + '~' + str(gain_limite))
+    loss_limite *= leverage
+    gain_limite *= leverage
+    record_to_file(f"gou_init\t{instId}\t{loss_limite:.3%}~{gain_limite:.3%}")
 
 def get_avail_currency(ccy = 'USDT'):
     result = accountAPI.get_currency(ccy)
     data2 = json.loads(json.dumps(result))
     return float(data2['data'][0]['details'][0]['availEq'])
-    record_to_file("get_avail_currency\t" + instId + "\t" + str(current_price))
+    record_to_file(f"get_avail_currency\t{instId}\t{current_price}")
 
 def get_coin_price():
     global current_price
     result = spotAPI.get_specific_ticker(instId)
     current_price = float(json.loads(json.dumps(result))['data'][0]['last'])
     current_change_rate = current_price / buy_price
-    record_to_file("get_coin_price\t" + instId + "\t" + str(current_price) + '/' + str(buy_price) + "\t" + str(round((current_price / buy_price - 1) * 100, 3)) +'%')
+    record_to_file(f"get_coin_price\t{instId}\t{current_price}/{buy_price}\t{(current_price / buy_price)}")
     return current_price
 
 def take_order(side):
     if side == 'buy':
-        sz = get_avail_currency() * 0.6
+        sz = get_avail_currency() * 0.3
     if side == 'sell':
-        sz = get_avail_currency() / get_coin_price() * 0.6
+        sz = get_avail_currency() / get_coin_price() * 0.3
     result = levelAPI.take_order(instId, 'cross', 'USDT', side, "market", sz)
-    record_to_file("take_order\t" + instId + "\t" + side + "\t" + str(sz) + "\t" + json.dumps(result))
+    record_to_file(f"take_order\t{instId}\t{side}\t{sz}\t" + json.dumps(result))
 
 def get_position_price():
-    global buy_price, deal_side, current_price
+    global buy_price, deal_side, current_price, upl_ratio
     buy_price = -1
     result = accountAPI.account_positions(instId)
     for position in json.loads(json.dumps(result))['data']:
@@ -63,67 +67,40 @@ def get_position_price():
                 deal_side = 'buy'
             if instId.endswith(str(position['posCcy'])):
                 deal_side = 'sell'
-    record_to_file("get_position_price\t" + instId + "\t" + str(buy_price) + "\t" + deal_side)
+    record_to_file(f"get_position_price\t{instId}\t{current_price}/{buy_price}\t{deal_side}\t{upl_ratio:.3%}")
     return buy_price
 
 def close_position():
     result = levelAPI.close_position(instId, ccy = 'USDT')     # 市价全平
-    record_to_file("close_position\t" + instId + "\t" + json.dumps(result))
+    record_to_file(f"close_position\t{instId}\t" + json.dumps(result))
 
 def record_to_file(content):
-    print(content)
+    print(f"{datetime.datetime.now().isoformat(' ', 'milliseconds')}\t{content}")
     file_path = os.getcwd() + '/gou_record/' + gouId + '.txt'
     with open(file_path, 'a+') as f:
-        f.write(str(datetime.datetime.now()) + "\t" + content + '\n')
+        f.write(f"{datetime.datetime.now().isoformat(' ', 'milliseconds')}\t{content}\n")
         f.close()
     
 def take():
-    global buy_price, current_price, losted_times, loss_limite, gain_limite
-    buy_price = get_position_price()
+    global buy_price, current_price, losted_times, loss_limite, gain_limite, upl_ratio
+    get_position_price()
     if buy_price < 0:
+        record_to_file("Can't_get_the_buy_price\t" + instId)
         time.sleep(10)
         return
     
-    # 获取当前价格
-    current_price = get_coin_price()
-    current_change_rate = current_price / buy_price
-    if deal_side == 'buy':       #做多
-        if current_change_rate < 1 - loss_limite:
-            # 市价全平
-            close_position()
-            losted_times += 1
-            record_to_file("losted_times\t" + str(losted_times))
-            if losted_times > 2:
-                time.sleep(1800)
-            #市价卖出 做空
-            take_order("sell")
-            #获取最新买入价buy_price
-            get_position_price()
-        elif current_change_rate > 1 + gain_limite:
-            # 市价全平
-            close_position()
-            losted_times = 0
-            #市价买入 做多
-            take_order("buy")
-            #获取最新买入价buy_price
-            get_position_price()
-    else:       #做空
-        if current_change_rate > 1 + loss_limite:
-            # 市价全平
-            close_position()
-            losted_times += 1
-            record_to_file("losted_times\t" + str(losted_times))
-            if losted_times > 2:
-                time.sleep(1800)
-            #市价买入 做多
-            take_order("buy")
-            #获取最新买入价buy_price
-            get_position_price()
-        elif current_change_rate < 1 - gain_limite:
-            # 市价全平
-            close_position()
-            losted_times = 0
-            #市价卖出 做空
-            take_order("sell")
-            #获取最新买入价buy_price
-            get_position_price()
+    if upl_ratio < loss_limite:
+        # 市价全平
+        close_position()
+        losted_times += 1
+        record_to_file("losted_times\t" + str(losted_times))
+        if losted_times > 2:
+            time.sleep(1800)
+        #转换买入方向
+        take_order("sell" if deal_side == "buy" else "buy")
+    elif upl_ratio > gain_limite:
+        # 市价全平
+        close_position()
+        losted_times = 0
+        #继续买入之前的方向
+        take_order(deal_side)
